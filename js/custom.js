8caa7d9793251bf9665e758d6d2a2cbfda16a937
iambadhon <toufiqul.dev@gmail.com>
2022-07-23 01:02:57 +0600

    something add

--- js/custom.js
+++ js/custom.js
@@ -8,20 +8,34 @@ $(function () {
 
   $(window).scroll(function () {
     var scrolling = $(this).scrollTop()
-
     if (scrolling > 100) {
       $(".scroll_top").fadeIn(800);
     } else {
       $(".scroll_top").fadeOut(800);
     }
+  });
+  //=====================scroll top end=====================//
 
+  //=====================sticky nav start=====================//
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop()
     if (scrolling > 100) {
       $(".main_nav").addClass("nav_sticky");
     } else {
       $(".main_nav").removeClass("nav_sticky");
     }
   });
-  //=====================scroll top end=====================//
+  //=====================sticky nav end=====================//
+
+  //=====================nav hide star=====================//
+  let navBar = document.querySelectorAll('.nav-link');
+  let navCollapse = document.querySelector('.navbar-collapse.collapse');
+  navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+      navCollapse.classList.remove("show");
+    })
+  })
+  //=====================nav hide end=====================//
 
   //=====================typed section start=====================//
   $(".typed").typed({
